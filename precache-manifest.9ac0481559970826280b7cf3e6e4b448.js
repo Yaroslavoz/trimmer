55e3cda315553abb51712e5effe6ecdcb9c9fec7
yaroslavoz <yaroslavozv@gmail.com>
2020-05-03 23:52:14 +0300

    Updates

---
+++ precache-manifest.9ac0481559970826280b7cf3e6e4b448.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8744b1a1a9cfc748d41db0c0dd00c30",
+    "url": "/trimmer/index.html"
+  },
+  {
+    "revision": "d75e707e0de9d4153d3d",
+    "url": "/trimmer/static/css/main.07449a5d.chunk.css"
+  },
+  {
+    "revision": "edba237fcd6a2e4c696d",
+    "url": "/trimmer/static/js/2.091c58bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/trimmer/static/js/2.091c58bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d75e707e0de9d4153d3d",
+    "url": "/trimmer/static/js/main.f3f9ed94.chunk.js"
+  },
+  {
+    "revision": "1f1a6f175bf3018caa07",
+    "url": "/trimmer/static/js/runtime-main.81fb30da.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/trimmer/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- src/Trimmer.js
+++
@@ -1,6 +0,0 @@
-const str = '380 (97) 517-37-98'
-const trimmer =(el) => {
-  
-return  Number(el.replace(/\(|\)|\+|\ |\-/g, ''))
-}
-console.log(trimmer(str));
